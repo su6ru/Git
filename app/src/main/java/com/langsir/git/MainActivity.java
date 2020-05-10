b5902cb57f6bc68a2087ce6c64040702ecde9ec5
su6ru123 <su6ru123@gmaul.com>
2020-05-10 17:35:34 +0800

    first

--- app/src/main/java/com/langsir/git/MainActivity.java
+++ app/src/main/java/com/langsir/git/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    int h=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        h=1;
+        h=3;
     }
 }
---
+++ app/src/main/java/com/langsir/git/R2.java
@@ -0,0 +1,4 @@
+package com.langsir.git;
+
+public class R2 {
+}

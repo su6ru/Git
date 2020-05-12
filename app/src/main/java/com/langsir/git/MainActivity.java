f9bb5019ec792680b4209dffee524c0390876dc0
su6ru123 <su6ru123@gmaul.com>
2020-05-12 11:54:06 +0800

    ms

--- app/src/main/java/com/langsir/git/MainActivity.java
+++ app/src/main/java/com/langsir/git/MainActivity.java
@@ -1,12 +1,14 @@
 package com.langsir.git;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     int h=0;
     int version=4;
+    String s="service";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
         String Master="Master";
         System.out.println(Master);
         System.out.println(g);
+        Log.d("branch","Master");
     }
 }
